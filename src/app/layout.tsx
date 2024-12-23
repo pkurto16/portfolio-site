cec1fb3bc896a6a9acf820821a7ecfe74f4a46dd
pkurto16 <ptkurto3@gmail.com>
2024-12-23 01:32:20 -0500

    Add analytics and smooth scrolling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import StoreProvider from './StoreProvider'
 import { Toaster } from "@/components/ui/toaster"
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -22,6 +23,7 @@ export default function RootLayout({
         <StoreProvider>
             {children}
             <Toaster />
+            <Analytics />
         </StoreProvider>
         </body>
         </html>
